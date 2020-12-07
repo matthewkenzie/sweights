c2c210ff3ed2626b44f914998b4b7cef7af2dcfb
Matthew Kenzie <matthew.kenzie@cern.ch>
2020-12-07 22:54:04 +0000

    got berns up to 6

--- cow_study/run.py
+++ cow_study/run.py
@@ -9,274 +9,258 @@ from iminuit import Minuit
 
 # B(n,i)(x) = (n!)/(i!*(n-i)!) * (1-x)^(n-i) * x^i
 
+def bernk(n,k,x):
+  return binom(n,k) * (1-x)**(n-k) * x**k
+
+def bern(x,*coeffs):
+  n = len(coeffs)-1
+  res = 0
+  for i in range(len(coeffs)):
+    res += coeffs[i] * bernk(n,i,x)
+  return res
+
+def bernI(x,*coeffs):
+  n = len(coeffs)-1
+  res = 0
+  for i in range(n+1):
+    term = 0
+    for j in range(i,n+1):
+      term += ((-1)**(j-i)) * binom(n,j) * binom(j,i) * (x**(j+1)) / (j+1)
+    term *= coeffs[i]
+    res += term
+  return res
+
+def bernN(*coeffs):
+    return 1/bernI(1,*coeffs)
+
 class bpoly1_gen(rv_continuous):
-  def _pdf(self, x, p0, p1):
-    N = 1/(0.5*p0 + 0.5*p1)
-    def f(x,p0,p1):
-      return p0*(1-x) + p1*x
-    return N * _lazywhere( ( x>=0) & ( x<=1 ) , (x,p0,p1), f=f, f2=lambda x, p0, p1: 0 )
-  def _cdf(self, x, p0, p1):
-    N = 1/(0.5*p0 + 0.5*p1)
-    def f(x,p0,p1):
-      return p0*x + (p1-p0)*x**2 / 2
-    ret = N * _lazywhere( ( x>=0 ) & ( x<=1 ), (x,p0,p1), f=f, f2=lambda x, p0, p1: 0 )
+  def _pdf(self,x,p0,p1):
+    N = bernN(p0,p1)
+    return N * _lazywhere( (x>=0) & (x<=1) , (x,p0,p1), f=lambda x,p0,p1: bern(x,p0,p1), f2=lambda x,p0,p1: 0 )
+  def _cdf(self,x,p0,p1):
+    N = bernN(p0,p1)
+    ret = N * _lazywhere( (x>=0) & (x<=1), (x,p0,p1), f=lambda x,p0,p1: bernI(x,p0,p1), f2=lambda x,p0,p1: 0 )
     ret[x>1] = 1
     return ret
-
-bpoly1 = bpoly1_gen(name='bern1',longname='bern1')
+bpoly1 = bpoly1_gen(name='bern1')
 
 class bpoly2_gen(rv_continuous):
-  def _pdf(self, x, p0, p1, p2):
-    N = 1/(p0+(p1-p0)+(p2-2*p1+p0)/3)
-    def f(x,p0,p1,p2):
-      return p0*(1-x)**2 + p1*2*x*(1-x) + p2*x**2
-    return N * _lazywhere( ( x>=0) & ( x<=1 ) , (x,p0,p1,p2), f=f, f2=lambda *args: 0 )
-  def _cdf(self, x, p0, p1, p2):
-    N = 1/(p0+(p1-p0)+(p2-2*p1+p0)/3)
-    def f(x,p0,p1,p2):
-      return p0*x + (p1-p0)*x**2 + (p2-2*p1+p0)*x**3 / 3
-    ret = N * _lazywhere( ( x>=0 ) & ( x<=1 ), (x,p0,p1,p2), f=f, f2=lambda *args: 0 )
+  def _pdf(self,x,p0,p1,p2):
+    N = bernN(p0,p1,p2)
+    return N * _lazywhere( (x>=0) & (x<=1) , (x,p0,p1,p2), f=lambda x,p0,p1,p2: bern(x,p0,p1,p2), f2=lambda x,p0,p1,p2: 0 )
+  def _cdf(self,x,p0,p1,p2):
+    N = bernN(p0,p1,p2)
+    ret = N * _lazywhere( (x>=0) & (x<=1), (x,p0,p1,p2), f=lambda x,p0,p1,p2: bernI(x,p0,p1,p2), f2=lambda x,p0,p1,p2: 0 )
     ret[x>1] = 1
     return ret
-
-bpoly2 = bpoly2_gen(name='bern2',longname='bern2')
+bpoly2 = bpoly2_gen(name='bern2')
 
 class bpoly3_gen(rv_continuous):
-  def _pdf(self, x, p0, p1, p2, p3):
-    N = 1/(p0+3*(p1-p0)/2+(p2-2*p1+p0)+(p3-3*p2+3*p1-p0)/4)
-    def f(x,p0,p1,p2,p3):
-      return p0*(1-x)**3 + p1*3*x*(1-x)**2 + p2*3*x**2*(1-x) + p3*x**3
-    return N * _lazywhere( ( x>=0) & ( x<=1 ) , (x,p0,p1,p2,p3), f=f, f2=lambda *args: 0 )
-  def _cdf(self, x, p0, p1, p2, p3):
-    N = 1/(p0+3*(p1-p0)/2+(p2-2*p1+p0)+(p3-3*p2+3*p1-p0)/4)
-    def f(x,p0,p1,p2,p3):
-      return p0*x + 3*(p1-p0)*x**2 / 2 + (p2-2*p1+p0)*x**3 + (p3-3*p2+3*p1-p0)*x**4 / 4
-    ret = N * _lazywhere( ( x>=0 ) & ( x<=1 ), (x,p0,p1,p2,p3), f=f, f2=lambda *args: 0 )
+  def _pdf(self,x,p0,p1,p2,p3):
+    N = bernN(p0,p1,p2,p3)
+    return N * _lazywhere( (x>=0) & (x<=1) , (x,p0,p1,p2,p3), f=lambda x,p0,p1,p2,p3: bern(x,p0,p1,p2,p3), f2=lambda x,p0,p1,p2,p3: 0 )
+  def _cdf(self,x,p0,p1,p2,p3):
+    N = bernN(p0,p1,p2,p3)
+    ret = N * _lazywhere( (x>=0) & (x<=1), (x,p0,p1,p2,p3), f=lambda x,p0,p1,p2,p3: bernI(x,p0,p1,p2,p3), f2=lambda x,p0,p1,p2,p3: 0 )
     ret[x>1] = 1
     return ret
-
-bpoly3 = bpoly3_gen(name='bern3',longname='bern3')
+bpoly3 = bpoly3_gen(name='bern3')
 
 class bpoly4_gen(rv_continuous):
-  def _pdf(self, x, p0, p1, p2, p3, p4):
-    N = 1/(p0+4*(p1-p0)/2+6*(p2-2*p1+p0)/3+4*(p3-3*p2+3*p1-p0)/4+(p4-4*p3+6*p2-4*p1+p0)/5)
-    def f(x,p0,p1,p2,p3,p4):
-      return p0*(1-x)**4 + p1*4*x*(1-x)**3 + p2*6*x**2*(1-x)**2 + p3*4*x**3*(1-x) + p4*x**4
-    return N * _lazywhere( ( x>=0) & ( x<=1 ) , (x,p0,p1,p2,p3,p4), f=f, f2=lambda *args: 0 )
-  def _cdf(self, x, p0, p1, p2, p3, p4):
-    N = 1/(p0+4*(p1-p0)/2+6*(p2-2*p1+p0)/3+4*(p3-3*p2+3*p1-p0)/4+(p4-4*p3+6*p2-4*p1+p0)/5)
-    def f(x,p0,p1,p2,p3,p4):
-      return p0*x + 4*(p1-p0)*x**2 / 2 + 6*(p2-2*p1+p0)*x**3 / 3 + (p3-3*p2+3*p1-p0)*x**4 + (p4-4*p3+6*p2-4*p1+p0)*x**5 / 5
-    ret = N * _lazywhere( ( x>=0 ) & ( x<=1 ), (x,p0,p1,p2,p3,p4), f=f, f2=lambda *args: 0 )
+  def _pdf(self,x,p0,p1,p2,p3,p4):
+    pars = [p0,p1,p2,p3,p4]
+    xpars = [x]+pars
+    N = bernN(*pars)
+    return N * _lazywhere( (x>=0) & (x<=1) , xpars, f=lambda *xpars: bern(*xpars), f2=lambda *xpars: 0 )
+  def _cdf(self,x,p0,p1,p2,p3,p4):
+    pars = [p0,p1,p2,p3,p4]
+    xpars = [x]+pars
+    N = bernN(*pars)
+    ret = N * _lazywhere( (x>=0) & (x<=1), xpars, f=lambda *xpars: bernI(*xpars), f2=lambda *xpars: 0 )
     ret[x>1] = 1
     return ret
+bpoly4 = bpoly4_gen(name='bern4')
 
-bpoly4 = bpoly4_gen(name='bern4',longname='bern4')
+class bpoly5_gen(rv_continuous):
+  def _pdf(self,x,p0,p1,p2,p3,p4,p5):
+    pars = [p0,p1,p2,p3,p4,p5]
+    xpars = [x]+pars
+    N = bernN(*pars)
+    return N * _lazywhere( (x>=0) & (x<=1) , xpars, f=lambda *xpars: bern(*xpars), f2=lambda *xpars: 0 )
+  def _cdf(self,x,p0,p1,p2,p3,p4,p5):
+    pars = [p0,p1,p2,p3,p4,p5]
+    xpars = [x]+pars
+    N = bernN(*pars)
+    ret = N * _lazywhere( (x>=0) & (x<=1), xpars, f=lambda *xpars: bernI(*xpars), f2=lambda *xpars: 0 )
+    ret[x>1] = 1
+    return ret
+bpoly5 = bpoly5_gen(name='bern5')
 
-#class bpoly(rv_continuous):
-  #def _pdf(self, x, *coeffs):
-    ##x = (x-range[0])/(range[1]-range[0])
-    #n = len(coeffs)-1
+class bpoly6_gen(rv_continuous):
+  def _pdf(self,x,p0,p1,p2,p3,p4,p5,p6):
+    pars = [p0,p1,p2,p3,p4,p5,p6]
+    xpars = [x]+pars
+    N = bernN(*pars)
+    return N * _lazywhere( (x>=0) & (x<=1) , xpars, f=lambda *xpars: bern(*xpars), f2=lambda *xpars: 0 )
+  def _cdf(self,x,p0,p1,p2,p3,p4,p5,p6):
+    pars = [p0,p1,p2,p3,p4,p5,p6]
+    xpars = [x]+pars
+    N = bernN(*pars)
+    ret = N * _lazywhere( (x>=0) & (x<=1), xpars, f=lambda *xpars: bernI(*xpars), f2=lambda *xpars: 0 )
+    ret[x>1] = 1
+    return ret
+bpoly6 = bpoly6_gen(name='bern6')
 
-    #if ( n == 0 ):
-      #return coeffs[0]
-    #elif ( n == 1 ):
-      #return (coeffs[1]-coeffs[0])*x + coeffs[0]
-    #elif ( n == 2 ):
-      #a0 = coeffs[0]
-      #a1 = 2 * (coeffs[1]-coeffs[0])
-      #a2 = coeffs[2] - a1 - a0
-      #return ( a2*x + a1 ) * x + a0
-    #elif ( n > 2 ):
-      #t = x
-      #s = 1-x
-      #res = coeffs[0] * s
-      #for i in range(1,n):
-        #res += t * binom(n,i) * coeffs[i] * s
-        #t *= x
-      #res += t * coeffs[n]
-      #return res
-    #return coeffs[0]
-  #def _cdf(self, x, *coeffs):
-    ##x = (x-range[0])/(range[1]-range[0])
-    #n = len(coeffs)-1
-    #res = 0
-    #for i in range(n+1):
-      #temp = 0
-      #for j in range(1,n+1):
-        #temp += (-1**(j-i)) * binom(n,j) * binom(j,i) * (x**(j+1)) / (j+1)
-      #temp *= coeffs[i]
-      #norm += temp
-    #return norm
+if __name__ == '__main__':
+  mrange = (5000,5600)
+  trange = (0,10)
+  g0 = norm(5280,30)
+  g1 = expon(5000,400)
+  h0 = expon(0,2)
+  h1 = norm(0,0.5)
+  z0 = 0.5
+  N = 10000
 
-mrange = (5000,5600)
-g0 = norm(5280,30)
-g1 = expon(5000,400)
-z0 = 0.5
-N = 10000
+  import pandas as pd
+  np.random.seed(210187)
 
-import pandas as pd
-np.random.seed(210187)
+  # generate toy
+  generate = False
+  fit = False
+  if generate:
+    data = pd.DataFrame(columns=['mass','time','ctrl'])
 
-# generate toy
-generate = False
-if generate:
-  data = pd.DataFrame(columns=['mass','ctrl'])
+    n_sig = np.random.poisson(N*z0)
+    n_bkg = np.random.poisson(N*(1-z0))
 
-  n_sig = np.random.poisson(N*z0)
-  n_bkg = np.random.poisson(N*(1-z0))
+    ns = 0
+    while ns < n_sig:
+      m = g0.rvs()
+      t = h0.rvs()
+      if m > mrange[1] or m < mrange[0]: continue
+      if t > trange[1] or t < trange[0]: continue
+      data = data.append( {'mass': m, 'time': t, 'ctrl': 0}, ignore_index=True )
+      ns += 1
 
-  ns = 0
-  while ns < n_sig:
-    m = g0.rvs()
-    if m > mrange[1] or m < mrange[0]: continue
-    data = data.append( {'mass': m, 'ctrl': 0}, ignore_index=True )
-    ns += 1
+    nb = 0
+    while nb < n_bkg:
+      m = g1.rvs()
+      t = h1.rvs()
+      if m > mrange[1] or m < mrange[0]: continue
+      if t > trange[1] or t < trange[0]: continue
+      data = data.append( {'mass': m, 'time': t, 'ctrl': 1}, ignore_index=True )
+      nb += 1
 
-  nb = 0
-  while nb < n_bkg:
-    m = g1.rvs()
-    if m > mrange[1] or m < mrange[0]: continue
-    data = data.append( {'mass': m, 'ctrl': 1}, ignore_index=True )
-    nb += 1
+    data = data.astype( {'mass': float, 'time': float, 'ctrl': int} )
+    data.to_pickle('toy.pkl')
 
-  data = data.astype( {'mass': float, 'ctrl': int} )
-  data.to_pickle('toy.pkl')
+  else:
+    data = pd.read_pickle('toy.pkl')
 
-else:
-  data = pd.read_pickle('toy.pkl')
+  # draw toy
+  fig, ax = plt.subplots(1,2, figsize=(12,4))
+  sig = data[data['ctrl']==0]
+  bkg = data[data['ctrl']==1]
 
-# fit the toy
-#def bpoly(coeffs, range=[0,1]):
-  #def pdf(x):
-    #x = (x-range[0])/(range[1]-range[0])
-    #n = len(coeffs)-1
+  ax[0].hist( [bkg['mass'],sig['mass']], bins=50, stacked=True )
+  ax[1].hist( [bkg['time'],sig['time']], bins=50, stacked=True )
+  ax[1].set_yscale('log')
+  fig.tight_layout()
 
-    #if ( n == 0 ):
-      #return coeffs[0]
-    #elif ( n == 1 ):
-      #return (coeffs[1]-coeffs[0])*x + coeffs[0]
-    #elif ( n == 2 ):
-      #a0 = coeffs[0]
-      #a1 = 2 * (coeffs[1]-coeffs[0])
-      #a2 = coeffs[2] - a1 - a0
-      #return ( a2*x + a1 ) * x + a0
-    #elif ( n > 2 ):
-      #t = x
-      #s = 1-x
-      #res = coeffs[0] * s
-      #for i in range(1,n):
-        #res += t * binom(n,i) * coeffs[i] * s
-        #t *= x
-      #res += t * coeffs[n]
-      #return res
-    #return coeffs[0]
-  #return pdf
-  #def cdf(x):
-    #x = (x-range[0])/(range[1]-range[0])
-    #n = len(coeffs)-1
-    #res = 0
-    #for i in range(n+1):
-      #temp = 0
-      #for j in range(1,n+1):
-        #temp += (-1**(j-i)) * binom(n,j) * binom(j,i) * (x**(j+1)) / (j+1)
-      #temp *= coeffs[i]
-      #norm += temp
-    #return norm
-  #return cdf
+  def nll_exp(N, z, mu, sg, lb):
+    s = norm(mu,sg)
+    b = expon(mrange[0], lb)
+    sn = np.diff( s.cdf(mrange) )
+    bn = np.diff( b.cdf(mrange) )
+    ns = N*z
+    nb = N*(1-z)
+    return N - np.sum( np.log ( s.pdf( data['mass'].to_numpy() ) / sn * ns + b.pdf( data['mass'].to_numpy() ) / bn * nb ) )
 
-def nll_exp(N, z, mu, sg, lb):
-  s = norm(mu,sg)
-  b = expon(mrange[0], lb)
-  sn = np.diff( s.cdf(mrange) )
-  bn = np.diff( b.cdf(mrange) )
-  ns = N*z
-  nb = N*(1-z)
-  return N - np.sum( np.log ( s.pdf( data['mass'].to_numpy() ) / sn * ns + b.pdf( data['mass'].to_numpy() ) / bn * nb ) )
+  def nll_poly(pars):
+    N  = pars[0]
+    z  = pars[1]
+    mu = pars[2]
+    sg = pars[3]
+    s = norm(mu,sg)
+    sn = np.diff( s.cdf(mrange) )
+    n = len(pars[4:])
+    if n==1: b  = bpoly1(1,pars[4],5000,600)
+    if n==2: b  = bpoly2(1,pars[4],pars[5],5000,600)
+    if n==3: b  = bpoly3(1,pars[4],pars[5],pars[6],5000,600)
+    if n==4: b  = bpoly4(1,pars[4],pars[5],pars[6],pars[7],5000,600)
 
-def nll_poly(pars):
-  N  = pars[0]
-  z  = pars[1]
-  mu = pars[2]
-  sg = pars[3]
-  s = norm(mu,sg)
-  sn = np.diff( s.cdf(mrange) )
-  n = len(pars[4:])
-  if n==1: b  = bpoly1(1,pars[4],5000,600)
-  if n==2: b  = bpoly2(1,pars[4],pars[5],5000,600)
-  if n==3: b  = bpoly3(1,pars[4],pars[5],pars[6],5000,600)
-  if n==4: b  = bpoly4(1,pars[4],pars[5],pars[6],pars[7],5000,600)
+    bn = np.diff( b.cdf(mrange) )
+    ns = N*z
+    nb = N*(1-z)
+    return N - np.sum( np.log ( s.pdf( data['mass'].to_numpy() ) / sn * ns + b.pdf ( data['mass'].to_numpy() ) / bn * nb ) )
 
-  bn = np.diff( b.cdf(mrange) )
-  ns = N*z
-  nb = N*(1-z)
-  return N - np.sum( np.log ( s.pdf( data['mass'].to_numpy() ) / sn * ns + b.pdf ( data['mass'].to_numpy() ) / bn * nb ) )
+  mi = Minuit( nll_exp, N=N, z=z0, mu=5280, sg=30, lb=400, errordef=0.5, pedantic=False )
 
-mi = Minuit( nll_exp, N=N, z=z0, mu=5280, sg=30, lb=400, errordef=0.5, pedantic=False )
+  if fit:
+    mi.migrad()
+    mi.hesse()
+    print(mi.params)
 
-mi.migrad()
-mi.hesse()
-print(mi.params)
+  mips =  [ Minuit.from_array_func( nll_poly, (N,z0,5280,30,0.5)            , name=('N','z','mu','sg','p1')               , limit=((0.5*N,1.5*N),(0,1),(5200,5300),(0,50),(0,1)), errordef=0.5, pedantic=False ),
+            Minuit.from_array_func( nll_poly, (N,z0,5280,30,0.5,0.5)        , name=('N','z','mu','sg','p1','p2')          , limit=((0.5*N,1.5*N),(0,1),(5200,5300),(0,50),(0,1),(0,1)), errordef=0.5, pedantic=False ),
+            Minuit.from_array_func( nll_poly, (N,z0,5280,30,0.5,0.5,0.5)    , name=('N','z','mu','sg','p1','p2','p3')     , limit=((0.5*N,1.5*N),(0,1),(5200,5300),(0,50),(0,1),(0,1),(0,1)), errordef=0.5, pedantic=False ),
+            Minuit.from_array_func( nll_poly, (N,z0,5280,30,0.5,0.5,0.5,0.5), name=('N','z','mu','sg','p1','p2','p3','p4'), limit=((0.5*N,1.5*N),(0,1),(5200,5300),(0,50),(0,1),(0,1),(0,1),(0,1)), errordef=0.5, pedantic=False ) ]
 
-mips =  [ Minuit.from_array_func( nll_poly, (N,z0,5280,30,0.5)            , name=('N','z','mu','sg','p1')               , limit=((0.5*N,1.5*N),(0,1),(5200,5300),(0,50),(0,1)), errordef=0.5, pedantic=False ),
-          Minuit.from_array_func( nll_poly, (N,z0,5280,30,0.5,0.5)        , name=('N','z','mu','sg','p1','p2')          , limit=((0.5*N,1.5*N),(0,1),(5200,5300),(0,50),(0,1),(0,1)), errordef=0.5, pedantic=False ),
-          Minuit.from_array_func( nll_poly, (N,z0,5280,30,0.5,0.5,0.5)    , name=('N','z','mu','sg','p1','p2','p3')     , limit=((0.5*N,1.5*N),(0,1),(5200,5300),(0,50),(0,1),(0,1),(0,1)), errordef=0.5, pedantic=False ),
-          Minuit.from_array_func( nll_poly, (N,z0,5280,30,0.5,0.5,0.5,0.5), name=('N','z','mu','sg','p1','p2','p3','p4'), limit=((0.5*N,1.5*N),(0,1),(5200,5300),(0,50),(0,1),(0,1),(0,1),(0,1)), errordef=0.5, pedantic=False ) ]
-
-for mip in mips:
-  mip.migrad()
-  mip.hesse()
-  print(mip.params)
+  if fit:
+    for mip in mips:
+      mip.migrad()
+      mip.hesse()
+      print(mip.params)
 
 # draw pdf
-def pdf(m, bonly=False, sonly=False, poly=None):
+  def pdf(m, bonly=False, sonly=False, poly=None):
 
-  values = mi.values if poly is None else mips[poly-1].values
+    values = mi.values if poly is None else mips[poly-1].values
 
-  mu = values['mu']
-  sg = values['sg']
-  spdf = norm( mu, sg )
-  sn   = np.diff(spdf.cdf(mrange))
+    mu = values['mu']
+    sg = values['sg']
+    spdf = norm( mu, sg )
+    sn   = np.diff(spdf.cdf(mrange))
 
-  if poly is None:
-    bpdf = expon( 5000, mi.values['lb'] )
-    bn   = np.diff(bpdf.cdf(mrange))
-  else:
-    assert( type(poly)==int )
-    args = [ values['p%d'%(p+1)] for p in range(poly) ]
-    if poly == 1: bpdf = bpoly1(1,*args,5000,600)
-    if poly == 2: bpdf = bpoly2(1,*args,5000,600)
-    if poly == 3: bpdf = bpoly3(1,*args,5000,600)
-    if poly == 4: bpdf = bpoly4(1,*args,5000,600)
-    bn   = np.diff(bpdf.cdf(mrange))
+    if poly is None:
+      bpdf = expon( 5000, mi.values['lb'] )
+      bn   = np.diff(bpdf.cdf(mrange))
+    else:
+      assert( type(poly)==int )
+      args = [ values['p%d'%(p+1)] for p in range(poly) ]
+      if poly == 1: bpdf = bpoly1(1,*args,5000,600)
+      if poly == 2: bpdf = bpoly2(1,*args,5000,600)
+      if poly == 3: bpdf = bpoly3(1,*args,5000,600)
+      if poly == 4: bpdf = bpoly4(1,*args,5000,600)
+      bn   = np.diff(bpdf.cdf(mrange))
 
-  ns   = values['N']*values['z']
-  nb   = values['N']*(1-values['z'])
-  sr = spdf.pdf(m)
-  br = bpdf.pdf(m)
-  if sonly: return ns * sr / sn
-  if bonly: return nb * br / bn
-  return ns * sr / sn + nb * br / bn
+    ns   = values['N']*values['z']
+    nb   = values['N']*(1-values['z'])
+    sr = spdf.pdf(m)
+    br = bpdf.pdf(m)
+    if sonly: return ns * sr / sn
+    if bonly: return nb * br / bn
+    return ns * sr / sn + nb * br / bn
 
-fig = plt.figure()
-ax = fig.gca()
-bins = 50
-pn = (mrange[1]-mrange[0])/bins
-x = np.linspace(*mrange,400)
+  fig = plt.figure()
+  ax = fig.gca()
+  bins = 50
+  pn = (mrange[1]-mrange[0])/bins
+  x = np.linspace(*mrange,400)
 
-w, xe = np.histogram( data['mass'].to_numpy(), bins=50, range=mrange )
-cx = 0.5 * (xe[1:] + xe[:-1] )
+  w, xe = np.histogram( data['mass'].to_numpy(), bins=50, range=mrange )
+  cx = 0.5 * (xe[1:] + xe[:-1] )
 
-ax.errorbar( cx, w, w**0.5, fmt='ko')
-#ax.plot( x, pn*pdf(x,bonly=True), 'r--')
-ax.plot( x, pn*pdf(x))
-#ax.plot( x, pn*pdf(x,bonly=True,poly=1), 'g--')
-ax.plot( x, pn*pdf(x,poly=1))
-#ax.plot( x, pn*pdf(x,bonly=True,poly=2), 'g--')
-ax.plot( x, pn*pdf(x,poly=2))
-ax.plot( x, pn*pdf(x,poly=3))
-ax.plot( x, pn*pdf(x,poly=4))
+  ax.errorbar( cx, w, w**0.5, fmt='ko')
+  #ax.plot( x, pn*pdf(x,bonly=True), 'r--')
+  ax.plot( x, pn*pdf(x))
+  #ax.plot( x, pn*pdf(x,bonly=True,poly=1), 'g--')
+  ax.plot( x, pn*pdf(x,poly=1))
+  #ax.plot( x, pn*pdf(x,bonly=True,poly=2), 'g--')
+  ax.plot( x, pn*pdf(x,poly=2))
+  ax.plot( x, pn*pdf(x,poly=3))
+  ax.plot( x, pn*pdf(x,poly=4))
 
-fig.tight_layout()
-plt.show()
+  fig.tight_layout()
+  plt.show()
 
